32ca5f274322f6681d099354b584b8c12f68774b
jied83 <jied83@gmail.com>
2013-04-02 22:44:24 +0400

    some fixes
    Change-Id: Id7305c90a8158428332887f7b5c1c38fdafce4fb

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -56,35 +56,46 @@ esac
 
 case "$target" in
     "msm8660")
-     echo 1 > /sys/module/rpm_resources/enable_low_power/L2_cache
-     echo 1 > /sys/module/rpm_resources/enable_low_power/pxo
-     echo 2 > /sys/module/rpm_resources/enable_low_power/vdd_dig
-     echo 2 > /sys/module/rpm_resources/enable_low_power/vdd_mem
-     echo 1 > /sys/module/rpm_resources/enable_low_power/rpm_cpu
-     echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/suspend_enabled
-     echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/suspend_enabled
-     echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/suspend_enabled
-     echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/suspend_enabled
-     echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/idle_enabled
-     echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/idle_enabled
-     echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/idle_enabled
-     echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/idle_enabled
-     echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-     echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-     echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-     echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
+	 echo 1 > /sys/module/rpm_resources/enable_low_power/L2_cache
+	 echo 1 > /sys/module/rpm_resources/enable_low_power/pxo
+	 echo 2 > /sys/module/rpm_resources/enable_low_power/vdd_dig
+	 echo 2 > /sys/module/rpm_resources/enable_low_power/vdd_mem
+	 echo 1 > /sys/module/rpm_resources/enable_low_power/rpm_cpu
+	 echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/suspend_enabled
+	 echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/suspend_enabled
+	 echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/suspend_enabled
+	 echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/suspend_enabled
+	 echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/idle_enabled
+	 echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/idle_enabled
+	 echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/idle_enabled
+	 echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/idle_enabled
+	 echo 1 > /sys/module/pm_8660/modes/cpu0/power_collapse/suspend_enabled
+	 echo 1 > /sys/module/pm_8660/modes/cpu1/power_collapse/suspend_enabled
+	 echo 1 > /sys/module/pm_8660/modes/cpu0/standalone_power_collapse/suspend_enabled
+	 echo 1 > /sys/module/pm_8660/modes/cpu1/standalone_power_collapse/suspend_enabled
+	 echo 1 > /sys/module/pm_8660/modes/cpu0/power_collapse/idle_enabled
+	 echo 1 > /sys/module/pm_8660/modes/cpu1/power_collapse/idle_enabled
+	 echo 1 > /sys/module/pm_8660/modes/cpu0/standalone_power_collapse/idle_enabled
+	 echo 1 > /sys/module/pm_8660/modes/cpu1/standalone_power_collapse/idle_enabled
+	 echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+	 echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+	 echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+     echo 85 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
      echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
      echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-     echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-     echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-     chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-     chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-     chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-     chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-     chown root.system /sys/devices/system/cpu/mfreq
-     chmod 220 /sys/devices/system/cpu/mfreq
-     chown root.system /sys/devices/system/cpu/cpu1/online
-     chmod 664 /sys/devices/system/cpu/cpu1/online
+	 echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+	 echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+	 chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+	 chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+	 chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+	 chown root.system /sys/devices/system/cpu/mfreq
+	 chmod 220 /sys/devices/system/cpu/mfreq
+	 chown root.system /sys/devices/system/cpu/cpu1/online
+	 chmod 664 /sys/devices/system/cpu/cpu1/online
+	 chown root.system /sys/devices/system/cpu/cpufreq/ondemand/boostpulse
+	 chmod 664 /sys/devices/system/cpu/cpufreq/ondemand/boostpulse
         ;;
 esac
 
