68b9e1b45f9b8022e83f1550fa7708f8a40b4bbf
MIFL <mifl.pc@gmail.com>
2014-07-07 18:43:07 +0200

    Bluetooth: Enable Bluetooth LE for QCOM platform
    Add Bluetooth LE related xml as part of system image to enable
    Bleutooth LE at host side
    
    Change-Id: I35e5dae5acf8749e42ab5239d9416661f6f3ce8e

--- common.mk
+++ common.mk
@@ -344,7 +344,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Postrecoveryboot
 PRODUCT_COPY_FILES += \
